fd958bafd4d9b8acca652dcbd3714abebcbc5f8b
SmitShah199 <smitshah2019@gmail.com>
2023-05-21 17:05:37 -0600

    Update loginpage.php
    added basic html file

--- LoginPage/loginpage.php
+++ LoginPage/loginpage.php
@@ -1,3 +1,15 @@
-<?php
-echo "Test this is Smit testing merge request from desktop GitHub";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <!-- I will add the php side form later. -->
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
