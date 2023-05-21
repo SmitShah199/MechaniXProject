84136940fee08c2f5fa3a48cf0c1e2564b859faf
NEIL SHAH <133184173+Neilshah1003@users.noreply.github.com>
2023-05-21 17:45:38 -0600

    Update homepage.php
    Basic Bootstrap code for the homepage.

--- HomePage/homepage.php
+++ HomePage/homepage.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+    <!-- I am adding a title for the homepage which is MechaniX-->
+    <h2>Welcome to MechaniX</h2>
+</body>
+</html>
