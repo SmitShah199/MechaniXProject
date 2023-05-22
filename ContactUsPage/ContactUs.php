ed44eece9fc9a13ee14681a393ff62bcde241542
Rajvi2402 <102757080+Rajvi2402@users.noreply.github.com>
2023-05-21 23:27:06 -0600

    Create ContactUs.php

---
+++ ContactUsPage/ContactUs.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Contact Us - MechaniX</title>
+</head>
+<body>
+  <h1>Contact Us</h1>
+  <p>We're here to assist you! If you have any questions, feedback, or need support, please don't hesitate to reach out to us. Choose your preferred method of contact or explore our additional resources for self-help options.</p>
+
+  <h2>Contact Information</h2>
+  <ul>
+    <li>Phone: +1-800-MECHANI </li>
+    <li>Email: <a href="mailto:mechanix24x7@gmail.com">mechanix24x7@gmail.com </a></li>
+    <li>Physical Address: 24 Hamilton St, S4S6A7, Regina,SK</li>
+  </ul>
+
+  <h2>Contact Form</h2>
+  <form action="submit_contact_form.php" method="post">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br>
+
+    <label for="subject">Subject:</label>
+    <input type="text" id="subject" name="subject" required><br>
+
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" required></textarea><br>
+
+    <label for="phone">Phone Number (optional):</label>
+    <input type="text" id="phone" name="phone"><br>
+
+    <label for="vehicle">Vehicle Details (optional):</label>
+    <input type="text" id="vehicle" name="vehicle"><br>
+
+    <input type="submit" value="Submit">
+  </form>
+
+  <h2>Customer Support Channels</h2>
+  <ul>
+    <li>Phone Support: Call us during business hours at +1-800-MECHANI.</li>
+    <li>Email Support: Send an email to <a href="mailto:mechanix24x7@gmail.com">mechanix24x7@gmail.com</a>. We strive to respond within 24hrs.</li>
+    <li>Live Chat: Chat with our support team directly on our website 24hrs.</li>
+  </ul>
+
+  <h2>FAQ Section</h2>
+  <p>Find answers to common questions in our <a href="faq.html">FAQ section</a> before reaching out to us.</p>
+
+  <h2>Social Media</h2>
+  <ul>
+    <li>Facebook: <a href="www.Facebook.com">Facebook page</a></li>
+    <li>Twitter: <a href="www.Twitter.com"> Twitter page</a></li>
+    <li>Instagram: <a href="www.Instagram.com">Instagram page</a></li>
+  </ul>
+
+  <h2>Response Time</h2>
+  <p>We aim to respond to customer inquiries within 24hr. Your satisfaction is important to us, and we appreciate your patience.</p>
+
+
+  <h2>Privacy Policy</h2>
+  <p>Your privacy is important to us. Read our privacy
