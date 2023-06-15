3b6494bccc66456933f76e1f3ac65f9c65ba4dda
Swarnim Kumar <99140805+SWARNIMKUMAR@users.noreply.github.com>
2023-06-15 17:56:21 -0600

    Create History.php
    can you please review it

---
+++ History/History.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>History Page</title>
+    <style>
+        /* Add your custom CSS styles here */
+    </style>
+</head>
+<body>
+    <!-- Add your history content here -->
+    <h2>Payment History</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Amount</th>
+                <!-- Add more table headers as needed -->
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>2023-05-25</td>
+                <td>$100.00</td>
+                <!-- Add more table rows as needed -->
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
+
