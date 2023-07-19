46c9d74753caf2bee1cde0c60713da50e923b2b8
Rajvi2402 <102757080+Rajvi2402@users.noreply.github.com>
2023-07-18 21:45:39 -0600

    Create MechanicRouter.js
    added code for Mechanic router

---
+++ router/MechanicRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as MechanicController from "../controllers/MechanicController.js";
+const mechanicRouter=express.Router();
+mechanicRouter.get("/",MechanicController.allMechanics);
+mechanicRouter.post("/",MechanicController.addMeachanic);
+mechanicRouter.post("/login",MechanicController.login);
+mechanicRouter.get("/:email",MechanicController.mechanicGetByEmail);
+mechanicRouter.patch("/:id",MechanicController.updateMechanic);
+mechanicRouter.delete("/:id",MechanicController.deleteMechanic);
+export default mechanicRouter;
