681b7471713758481bd377be3296f33f8a0a35e2
SmitShah199 <66700299+SmitShah199@users.noreply.github.com>
2023-07-21 15:36:58 -0600

    Delete router directory

--- router/PaymentRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import* as PaymentController from '../controllers/PaymentController.js'
-const paymentRouter= express.Router();
-paymentRouter.post("/",PaymentController.addPayment)
-paymentRouter.get("/",PaymentController.all)
-paymentRouter.get("/mechanic/:email",PaymentController.mechanicPayments)
-paymentRouter.get("/customer/:email",PaymentController.customerPayments)
-export default paymentRouter;
